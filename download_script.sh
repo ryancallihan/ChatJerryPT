87968197b4c314ebc598bc2aac6df2c9ca8a6b45
Ryan Callihan <ryancallihan@gmail.com>
2023-08-22 08:03:16 +0000

    remove files

--- download_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget https://raw.githubusercontent.com/luonglearnstocode/Seinfeld-text-corpus/master/corpus.txt
-
-awk '/^JERRY: / { sub(/^JERRY: /, ""); print }' corpus.txt > jerry.txt
-
-rm corpus.txt
