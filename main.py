1eedcd064b5cfa7c4b0ec4a006dee4a9ac4582ed
Ryan Callihan <ryancallihan@gmail.com>
2023-08-23 19:42:34 +0000

    Updated langchain

--- main.py
+++ main.py
@@ -52,6 +52,7 @@ async def websocket_endpoint(websocket: WebSocket):
             result = await qa_chain.acall(
                 {"question": question, "chat_history": chat_history}
             )
+            print("\n\n\nRESULTS:::::::", result, "/n/n")
             chat_history.append((question, result["answer"]))
 
             end_resp = ChatResponse(sender="bot", message="", type="end")
